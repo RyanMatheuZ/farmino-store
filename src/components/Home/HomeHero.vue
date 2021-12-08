61145e6a026ca9575c582ba37315ecc0493f3135
Ryan Oliveira <oryan424@gmail.com>
2021-12-07 23:26:02 -0300

    Hero adjustments

--- src/components/Home/HomeHero.vue
+++ src/components/Home/HomeHero.vue
@@ -1,9 +1,9 @@
 <template>
 	<section class="bg-hero bg-cover bg-center">
-		<div class="container mx-auto">
+		<div class="bg-gray-100 md:bg-transparent bg-opacity-90 md:container md:mx-auto">
 			<article class="space-y-7 p-6 md:p-16">
 				<header>
-					<h1 class="text-black-100 text-4xl md:text-6xl font-bold space-y-4">
+					<h1 class="text-black-100 text-4xl md:text-6xl font-bold space-y-1 md:space-y-4">
 						<span class="block">
 							Junte-se à nós, entre
 						</span>
@@ -12,7 +12,7 @@
 							para o movimento
 						</span>
 
-						<span class="block uppercase">
+						<span class="uppercase block">
 							orgânico
 						</span>
 					</h1>
@@ -23,22 +23,22 @@
 						Saiba diferenciar o: VOCÊ QUER ou VOCÊ PRECISA?
 					</p>
 
-					<p class="text-yellow-medium font-semibold text-lg">
+					<p class="text-yellow-medium font-semibold text-md md:text-lg">
 						Pequenas mudanças fazem toda diferença no cotidiano do nosso planeta!
 					</p>
 				</div>
 
 				<RouterLink
 					to="/products"
-					class="flex items-center justify-between uppercase bg-green-dark hover:bg-green-light text-md text-white font-semibold rounded-md transition-all ease-in-out duration-300 w-max p-3"
+					class="flex items-center justify-between uppercase bg-green-dark hover:bg-green-light text-white text-md font-semibold rounded-md transition-all ease-in-out duration-300 w-max p-3"
 				>
 					<img
-						src="/icon/cart.svg"
+						src="/icon/cash.svg"
 						alt="Comprar agora"
 						title="Comprar agora"
 						draggable="false"
 						width="20"
-						class="mr-2"
+						class="mr-3"
 					>
 					comprar
 				</RouterLink>
--- src/components/Home/HomeOffers.vue
+++ src/components/Home/HomeOffers.vue
@@ -1,6 +1,6 @@
 <template>
-	<section class="flex flex-col items-center space-y-5 md:flex-row md:justify-center md:space-x-5 md:space-y-0 transform translate-y-10">
-		<article class="bg-blue-50 grid grid-cols-2 rounded-lg w-96 p-5">
+	<section class="flex flex-col items-center space-y-5 md:flex-row md:justify-center md:space-x-5 md:space-y-0 transform translate-y-10 mx-5 md:mx-0">
+		<article class="bg-blue-200 grid grid-cols-2 rounded-lg p-5">
 			<div class="flex flex-col justify-center">
 				<span class="uppercase text-green-800 text-xl font-bold">
 					25% OFF
@@ -18,18 +18,18 @@
 				</RouterLink>
 			</div>
 
-			<figure class="flex items-center justify-center">
-				<img 
-					src="/img/watermelon-image.jpg" 
+			<figure class="flex items-center justify-center mx-2">
+				<img
+					src="/img/watermelon-image.png"
 					alt="Frutas com desconto"
 					title="Frutas com desconto"
 					draggable="false"
-					width="150"
+					width="120"
 				>
 			</figure>
 		</article>
 
-		<article class="bg-yellow-100 grid grid-cols-2 rounded-lg w-96 p-5">
+		<article class="bg-yellow-200 grid grid-cols-2 rounded-lg p-5">
 			<div class="flex flex-col justify-center">
 				<span class="uppercase text-green-800 text-xl font-bold">
 					45% OFF
@@ -47,18 +47,18 @@
 				</RouterLink>
 			</div>
 
-			<figure class="flex items-center justify-center">
-				<img 
-					src="/img/potato-image.jpg" 
+			<figure class="flex items-center justify-center mx-2">
+				<img
+					src="/img/onion-image.png"
 					alt="Legumes com desconto"
 					title="Legumes com desconto"
 					draggable="false"
-					width="150"
+					width="120"
 				>
 			</figure>
 		</article>
 
-		<article class="bg-red-100 grid grid-cols-2 rounded-lg w-96 p-5">
+		<article class="bg-red-200 grid grid-cols-2 rounded-lg p-5">
 			<div class="flex flex-col justify-center">
 				<span class="uppercase text-green-800 text-xl font-bold">
 					30% OFF
@@ -76,13 +76,13 @@
 				</RouterLink>
 			</div>
 
-			<figure class="flex items-center justify-center">
-				<img 
-					src="/img/healthy-image.jpg" 
+			<figure class="flex items-center justify-center mx-2">
+				<img
+					src="/img/healthy-image.png"
 					alt="Verduras com desconto"
 					title="Verduras com desconto"
 					draggable="false"
-					width="150"
+					width="120"
 				>
 			</figure>
 		</article>
