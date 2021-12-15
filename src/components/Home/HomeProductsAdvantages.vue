b6095b24c234a0c1475351004f8cac7de498304a
Ryan Oliveira <oryan424@gmail.com>
2021-12-15 00:20:54 -0300

    Development of  products advantages

--- src/components/Home/HomeProductsAdvantages.vue
+++ src/components/Home/HomeProductsAdvantages.vue
@@ -1,47 +1,83 @@
 <template>
-	<section class="flex flex-col md:flex-row justify-between mt-20">
-		<article class="space-y-5 pl-10 md:pl-28 py-10">
-			<h2 class="text-black-100 font-semibold text-4xl">
-				Veja as vantagens <br> de nossos produtos
-			</h2>
+	<section>
+		<div class="container flex flex-col items-center justify-center xl:flex-row xl:items-start xl:space-x-32 mx-auto mt-20">
+			<article class="space-y-5 py-10">
+				<h2 class="text-black-100 font-semibold text-4xl">
+					Veja as vantagens <br> de nossos produtos
+				</h2>
 
-			<p class="text-black-100 w-80">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui repudiandae, vel incidunt quo dolorum recusandae, fugit eveniet possimus minus tempora quos quisquam voluptatem, non nobis quis molestias autem at voluptatum!
-			</p>
-
-			<div>
-				<RouterLink
-					to=""
-					class="text-green-dark text-lg font-bold"
-				>
-					Explorar
-				</RouterLink>
-			</div>
-		</article>
-
-		<div class="flex justify-between pr-10 md:pr-28 py-10">
-			<article class="align rounded-3xl shadow-lg w-72 py-5 px-8">
-				<figure>
-					<img
-						src="/img/peach-image.jpg"
-						alt="Pêssego"
-						title="Pêssego"
-						draggable="false"
-						width="200"
-						class="mx-auto"
-					>
-				</figure>
+				<p class="text-black-100 w-80">
+					Produtos 100% orgânicos e naturais, sem o uso de agrotóxicos ou pesticidas, ideais para sua saúde e boa alimentação!
+				</p>
 
 				<div>
-					<span class="text-3xl font-normal">
-						Pêssego
-					</span>
+					<RouterLink
+						to="/products"
+						class="flex justify-between text-green-dark text-lg font-bold max-w-max transform hover:translate-x-3 transition-transform duration-500 ease-in-out"
+					>
+						Explorar
 
-					<p class="text-black-100 mt-2">
-						Fruta originária da China e muito consumida pelo seu sabor doce.
-					</p>
+						<img
+							src="/icon/arrow-right.svg"
+							alt="Explorar"
+							title="Explorar"
+							draggable="false"
+							loading="lazy"
+							width="30"
+						>
+					</RouterLink>
 				</div>
 			</article>
+
+			<div class="flex flex-wrap justify-center py-10">
+				<article class="rounded-3xl shadow-lg w-72 mx-3 md:mx-5 md:mb-5 py-5 px-8">
+					<figure>
+						<img
+							src="/img/peach-image.jpg"
+							alt="Pêssego"
+							title="Pêssego"
+							draggable="false"
+							loading="lazy"
+							width="200"
+							class="mx-auto"
+						>
+					</figure>
+
+					<div>
+						<span class="text-yellow-600 text-3xl font-semibold">
+							Pêssego
+						</span>
+
+						<p class="text-black-100 mt-2">
+							Fruta originária da China e muito consumida pelo seu sabor doce.
+						</p>
+					</div>
+				</article>
+
+				<article class="rounded-3xl shadow-lg w-72 mx-3 md:mx-5 md:mt-5 py-5 px-8">
+					<figure>
+						<img
+							src="/img/tomato-image.jpg"
+							alt="Tomate"
+							title="Tomate"
+							draggable="false"
+							loading="lazy"
+							width="200"
+							class="mx-auto"
+						>
+					</figure>
+
+					<div>
+						<span class="text-red-600 text-3xl font-semibold">
+							Tomate
+						</span>
+
+						<p class="text-black-100 mt-2">
+							A palavra portuguesa tomate vem do castelhano tomate, derivada do náuatle tomatl.
+						</p>
+					</div>
+				</article>
+			</div>
 		</div>
 	</section>
 </template>
