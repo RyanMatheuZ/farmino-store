01b055cb3974fa4b8dde0704324640b7a3b9459a
Ryan Oliveira <oryan424@gmail.com>
2022-01-06 00:36:25 -0300

    AboutHero adjustments

--- src/components/About/AboutHero.vue
+++ src/components/About/AboutHero.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
 		<div class="container mx-auto">
-			<article class="space-y-10 p-6 md:p-16">
+			<article class="space-y-10 md:space-y-16 p-6 md:p-16">
 				<h1 class="text-black-100 text-4xl md:text-6xl font-bold space-y-1 md:space-y-2">
 					<span class="block">
 						Conheça agora, um
