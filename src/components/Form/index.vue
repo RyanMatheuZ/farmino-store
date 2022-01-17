30e68f0aefd90589eb01d0fd2d90263d79e4d13a
Ryan Oliveira <oryan424@gmail.com>
2022-01-17 15:11:53 -0300

    Form adjustments

--- src/components/Form/index.vue
+++ src/components/Form/index.vue
@@ -1,11 +1,58 @@
 <template>
 	<div class="container mx-auto px-6 md:px-16">
 		<form class="bg-black-100 border-green-dark border-l-4 rounded-lg px-3 py-4">
-			<FormInputName />
+			<div class="flex flex-col">
+				<label
+					for="input-name"
+					class="text-green-light text-lg font-semibold"
+				>
+					Nome:
+				</label>
 
-			<FormInputEmail />
+				<input
+					type="text"
+					id="input-name"
+					placeholder="Digite aqui seu nome"
+					class="outline-none ring-green-dark focus:ring-2 rounded-md my-3 p-3"
+				>
 
-			<FormInputMessage />
+				<span></span>
+			</div>
+
+			<div class="flex flex-col">
+				<label
+					for="input-email"
+					class="text-green-light text-lg font-semibold"
+				>
+					E-mail:
+				</label>
+
+				<input
+					type="email"
+					id="input-email"
+					placeholder="Digite aqui seu e-mail"
+					class="outline-none ring-green-dark focus:ring-2 rounded-md my-3 p-3"
+				>
+
+				<span></span>
+			</div>
+
+			<div class="flex flex-col">
+				<label
+					for="input-message"
+					class="text-green-light text-lg font-semibold"
+				>
+					Mensagem:
+				</label>
+
+				<textarea
+					id="input-message"
+					placeholder="Digite aqui sua mensagem"
+					class="outline-none resize-y ring-green-dark focus:ring-2 rounded-md mt-3 p-3"
+				></textarea>
+
+				<span></span>
+			</div>
 		</form>
 
 		<section class="grid grid-cols-1 md:grid-cols-3 gap-2 pt-2">
@@ -19,9 +66,6 @@
 </template>
 
 <script>
-const FormInputName = () => import('./FormInputName')
-const FormInputEmail = () => import('./FormInputEmail')
-const FormInputMessage = () => import('./FormInputMessage')
 const BoxAddress = () => import('./Box/BoxAddress')
 const BoxPhone = () => import('./Box/BoxPhone')
 const BoxEmail = () => import('./Box/BoxEmail')
@@ -30,9 +74,6 @@ export default {
 	name: 'Form',
 
 	components: {
-		FormInputName,
-		FormInputEmail,
-		FormInputMessage,
 		BoxAddress,
 		BoxPhone,
 		BoxEmail
